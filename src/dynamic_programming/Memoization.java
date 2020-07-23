7314049ba0beb7d8a2f09139f676f21da82fc3fa
Satyaprakash verma <59075705+blue-run@users.noreply.github.com>
2020-07-23 14:42:07 +0530

    memoization

---
+++ src/dynamic_programming/Memoization.java
@@ -0,0 +1,31 @@
+package dynamic_programming;
+
+import java.util.HashMap;
+
+public class Memoization {
+    public int addTo80(int n) {
+        System.out.println("long time");
+        return n + 80;
+    }
+
+    static HashMap<Integer, Integer> cache = new HashMap<>();
+
+    public int memoizeAddTo80(int n) {
+        // once cached it won't have to add every time the function is called.
+        if (!cache.containsKey(n)) {
+            System.out.println("long time");
+            cache.put(n, n + 80);
+        }
+        return cache.get(n);
+    }
+
+    public static void main(String[] args) {
+        Memoization memo = new Memoization();
+        System.out.println("addTo80: " + memo.addTo80(5));
+        System.out.println("addTo80: " + memo.addTo80(5));
+
+        System.out.println("memoizeAddTo80: " + memo.memoizeAddTo80(5));
+        System.out.println("memoizeAddTo80: " + memo.memoizeAddTo80(5));
+        System.out.println("memoizeAddTo80: " + memo.memoizeAddTo80(5));
+    }
+}
