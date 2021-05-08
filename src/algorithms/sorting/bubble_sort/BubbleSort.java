513816220ad194221d0c71f469d848be1b5719e2
Bruno Lima <brunopclima81@gmail.com>
2021-05-08 18:58:58 -0300

    some enhancement in BubbleSort Algorithm

--- src/algorithms/sorting/bubble_sort/BubbleSort.java
+++ src/algorithms/sorting/bubble_sort/BubbleSort.java
@@ -4,7 +4,7 @@ import java.util.Arrays;
 
 public class BubbleSort {
     public int[] bubbleSort(int[] array) {
-        for (int i = 0; i < array.length; i++) {
+        for (int i = 0; i < array.length - 1; i++) {
             for (int j = 0; j < array.length-1; j++) {
                 if (array[j] > array[j + 1]) {
                     int temp = array[j];
