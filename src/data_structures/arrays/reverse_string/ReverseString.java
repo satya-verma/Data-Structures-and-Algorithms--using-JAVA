93095ff95a8913b1dfad85a6378126c69b216d26
Rakshita Yadav <rakshitayadav@Rakshitas-MacBook-Air.local>
2023-11-01 18:41:54 +0000

    added another reverse method in ReverseString.java

--- src/data_structures/arrays/reverse_string/ReverseString.java
+++ src/data_structures/arrays/reverse_string/ReverseString.java
@@ -23,11 +23,34 @@ public class ReverseString {
         return temp.toString();
     }
 
+    //reverse method 3
+    private String reverse3(String str){
+        char[] myArray = str.toCharArray();
+        int left, right = 0;
+        right = myArray.length - 1;
+ 
+        for (left = 0; left < right; left++, right--) {
+            // Swap values of left and right
+            char temp = myArray[left];
+            myArray[left] = myArray[right];
+            myArray[right] = temp;
+        }
+
+        String outputString= String.valueOf(myArray);
+        return outputString;
+    }
+
     public static void main(String[] args) {
         // lets run our functions
         String greet = "Hello World";
         ReverseString reverseString = new ReverseString();
+        
         System.out.println(reverseString.reverse(greet));
         System.out.println(reverseString.reverse2(greet));
+        System.out.println(reverseString.reverse3(greet));
+
+        
     }
+
+
 }
